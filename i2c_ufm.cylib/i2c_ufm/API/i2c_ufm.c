6dfac9e0ba5b504414f48d3ab84017ed8562fe48
Kim Taylor <kmtaylor@gmx.com>
2020-11-16 23:48:15 +1100

    Use UDB based I2C UFM module.

--- i2c_ufm.cylib/i2c_ufm/API/i2c_ufm.c
+++ i2c_ufm.cylib/i2c_ufm/API/i2c_ufm.c
@@ -10,31 +10,31 @@ static uint8 setup_done;
 CY_ISR(`$INSTANCE_NAME`_ISR) {
     if (isr_buf_idx < isr_buf_size) {
         `$INSTANCE_NAME`_DATA_REG = isr_buf[isr_buf_idx++];
-        `$INSTANCE_NAME`_CSR_REG  = `$INSTANCE_NAME`_CSR_TRANSMIT;
+        `$INSTANCE_NAME`_GO_REG   = 0x00;
     } else {
-        `$INSTANCE_NAME`_MCSR_REG = `$INSTANCE_NAME`_MCSR_STOP_GEN;
-        `$INSTANCE_NAME`_CSR_REG  = `$INSTANCE_NAME`_CSR_TRANSMIT;
+        `$INSTANCE_NAME`_CFG_REG = 0x46; // Send stop
+        `$INSTANCE_NAME`_GO_REG  = 0x00;
     }
 }
 
 static void `$INSTANCE_NAME`_Config(void) {
     uint8 intState;
 
-    `$INSTANCE_NAME`_CFG_REG  = `$INSTANCE_NAME`_CFG_EN_MSTR;
-    `$INSTANCE_NAME`_XCFG_REG = `$INSTANCE_NAME`_XCFG_CLK_EN;
-    `$INSTANCE_NAME`_CLKDIV1_REG = LO8(`$INSTANCE_NAME`_DIVIDER);
-    `$INSTANCE_NAME`_CLKDIV2_REG = HI8(`$INSTANCE_NAME`_DIVIDER);
+    `$INSTANCE_NAME`_CFG_REG        = 0x00; // Disable module
+    `$INSTANCE_NAME`_INT_MASK_REG   = 0x01; // Unmask byte complete interrupt
 
-    /* Enable power to block */
+    /* Enable status register interrupt */
     intState = CyEnterCriticalSection();
-    `$INSTANCE_NAME`_ACT_PWRMGR_REG  |= `$INSTANCE_NAME`_ACT_PWR_EN;
-    `$INSTANCE_NAME`_STBY_PWRMGR_REG |= `$INSTANCE_NAME`_STBY_PWR_EN;
+    `$INSTANCE_NAME`_INT_ENABLE_REG |= 0x10;
     CyExitCriticalSection(intState);
+
+    `$INSTANCE_NAME`_MCLK_PRD_REG = 15;
+    `$INSTANCE_NAME`_MCLK_CMP_REG = 8;
 }
 
 void `$INSTANCE_NAME`_SetupDMA(uint8 max_len) {
     uint8 i, curr_td, next_td;
-    static uint8 dma_transmit = `$INSTANCE_NAME`_CSR_TRANSMIT;
+    static uint8 dma_transmit;
 
     if (setup_done) return;
 
@@ -52,7 +52,7 @@ void `$INSTANCE_NAME`_SetupDMA(uint8 max_len) {
         if ((i % 2) == 0) {
             CyDmaTdSetAddress(curr_td,
                     LO16((uint32) &dma_transmit),
-                    LO16((uint32) &`$INSTANCE_NAME`_CSR_REG));
+                    LO16((uint32) &`$INSTANCE_NAME`_GO_REG));
         }
         next_td = CyDmaTdAllocate();
         CyDmaTdSetConfiguration(curr_td, 1, next_td,
@@ -62,7 +62,7 @@ void `$INSTANCE_NAME`_SetupDMA(uint8 max_len) {
     CyDmaTdSetConfiguration(curr_td, 1, CY_DMA_DISABLE_TD, 0);
     CyDmaTdSetAddress(curr_td,
             LO16((uint32) &dma_transmit),
-            LO16((uint32) &`$INSTANCE_NAME`_CSR_REG));
+            LO16((uint32) &`$INSTANCE_NAME`_GO_REG));
 
     setup_done = 1;
 }
@@ -81,11 +81,12 @@ void `$INSTANCE_NAME`_SetupIRQ(void) {
 
 void `$INSTANCE_NAME`_WriteDMA(uint8 len, uint8 *wr_data) {
     uint8 i, curr_td, next_td;
-    static uint8 dma_stop = `$INSTANCE_NAME`_MCSR_STOP_GEN;
+    static uint8 dma_stop = 0x46; // Send stop
 
     if (!setup_done) return;
 
     CyDmaChDisable(dma_ch);
+    `$INSTANCE_NAME`_CFG_REG = 0x00; // Disable module
 
     curr_td = dma_td_first;
 
@@ -93,7 +94,7 @@ void `$INSTANCE_NAME`_WriteDMA(uint8 len, uint8 *wr_data) {
         if (i == len*2) {
             CyDmaTdSetAddress(curr_td,
                     LO16((uint32) &dma_stop),
-                    LO16((uint32) &`$INSTANCE_NAME`_MCSR_REG));
+                    LO16((uint32) &`$INSTANCE_NAME`_CFG_REG));
         } else if ((i % 2) == 0) {
             CyDmaTdSetAddress(curr_td,
                     LO16((uint32) &wr_data[i/2]),
@@ -108,8 +109,9 @@ void `$INSTANCE_NAME`_WriteDMA(uint8 len, uint8 *wr_data) {
     CyDmaChSetInitialTd(dma_ch, dma_td_first);
     CyDmaChEnable(dma_ch, 1);
 
+    `$INSTANCE_NAME`_CFG_REG  = 0x06; // Enable transmit
     `$INSTANCE_NAME`_DATA_REG = wr_data[0];
-    `$INSTANCE_NAME`_MCSR_REG = `$INSTANCE_NAME`_MCSR_START_GEN;
+    `$INSTANCE_NAME`_GO_REG   = 0;
 }
 
 void `$INSTANCE_NAME`_WriteIRQ(uint8 addr, uint8 len, uint8 *wr_data) {
@@ -119,7 +121,8 @@ void `$INSTANCE_NAME`_WriteIRQ(uint8 addr, uint8 len, uint8 *wr_data) {
     isr_buf_size = len;
     isr_buf      = wr_data;
 
-    /* Hardware actions: write address and generate Start or ReStart */
-    `$INSTANCE_NAME`_DATA_REG = addr << 1;
-    `$INSTANCE_NAME`_MCSR_REG = `$INSTANCE_NAME`_MCSR_START_GEN;
+    /* Hardware actions: write address and generate Start */
+    `$INSTANCE_NAME`_CFG_REG  = 0x06; // Enable transmit
+    `$INSTANCE_NAME`_DATA_REG = addr << 1;;
+    `$INSTANCE_NAME`_GO_REG   = 0;
 }
--- i2c_ufm.cylib/i2c_ufm/API/i2c_ufm.h
+++ i2c_ufm.cylib/i2c_ufm/API/i2c_ufm.h
@@ -1,34 +1,13 @@
+#define `$INSTANCE_NAME`_ISR_NUMBER     ((uint8) `$INSTANCE_NAME`_IRQ__INTC_NUMBER)
+#define `$INSTANCE_NAME`_ISR_PRIORITY   ((uint8) `$INSTANCE_NAME`_IRQ__INTC_PRIOR_NUM)
 
-/* XCFG I2C Extended Configuration Register */
-#define `$INSTANCE_NAME`_XCFG_CLK_EN        (0x80u)
-
-/* CFG I2C Configuration Register */
-#define `$INSTANCE_NAME`_CFG_EN_MSTR        (0x02u)
-
-/* CSR I2C Control and Status Register */
-#define `$INSTANCE_NAME`_CSR_TRANSMIT       (0x04u)
-
-/* MCSR I2C Master Control and Status Register */
-#define `$INSTANCE_NAME`_MCSR_STOP_GEN      (0x10u)
-#define `$INSTANCE_NAME`_MCSR_START_GEN     (0x01u)
-
-#define `$INSTANCE_NAME`_DIVIDER            ((BCLK__BUS_CLK__MHZ+15)/16)
-
-#define `$INSTANCE_NAME`_ISR_NUMBER         ((uint8) `$INSTANCE_NAME`_IRQ__INTC_NUMBER)
-#define `$INSTANCE_NAME`_ISR_PRIORITY       ((uint8) `$INSTANCE_NAME`_IRQ__INTC_PRIOR_NUM)
-
-#define `$INSTANCE_NAME`_ACT_PWR_EN         ((uint8) `$INSTANCE_NAME`_I2C_FF__PM_ACT_MSK)
-#define `$INSTANCE_NAME`_STBY_PWR_EN        ((uint8) `$INSTANCE_NAME`_I2C_FF__PM_STBY_MSK)
-
-#define `$INSTANCE_NAME`_ACT_PWRMGR_REG     (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__PM_ACT_CFG)
-#define `$INSTANCE_NAME`_STBY_PWRMGR_REG    (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__PM_STBY_CFG)
-#define `$INSTANCE_NAME`_XCFG_REG           (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__XCFG)
-#define `$INSTANCE_NAME`_CFG_REG            (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__CFG)
-#define `$INSTANCE_NAME`_CSR_REG            (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__CSR)
-#define `$INSTANCE_NAME`_DATA_REG           (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__D)
-#define `$INSTANCE_NAME`_MCSR_REG           (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__MCSR)
-#define `$INSTANCE_NAME`_CLKDIV1_REG        (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__CLK_DIV1)
-#define `$INSTANCE_NAME`_CLKDIV2_REG        (*(reg8 *) `$INSTANCE_NAME`_I2C_FF__CLK_DIV2)
+#define `$INSTANCE_NAME`_CFG_REG        (*(reg8 *) `$INSTANCE_NAME`_bI2C_UDB_SyncCtl_CtrlReg__CONTROL_REG)
+#define `$INSTANCE_NAME`_INT_MASK_REG   (*(reg8 *) `$INSTANCE_NAME`_bI2C_UDB_StsReg__MASK_REG)
+#define `$INSTANCE_NAME`_INT_ENABLE_REG (*(reg8 *) `$INSTANCE_NAME`_bI2C_UDB_StsReg__STATUS_AUX_CTL_REG)
+#define `$INSTANCE_NAME`_MCLK_PRD_REG   (*(reg8 *) `$INSTANCE_NAME`_bI2C_UDB_Master_ClkGen_u0__D0_REG)
+#define `$INSTANCE_NAME`_MCLK_CMP_REG   (*(reg8 *) `$INSTANCE_NAME`_bI2C_UDB_Master_ClkGen_u0__D1_REG)
+#define `$INSTANCE_NAME`_DATA_REG       (*(reg8 *) `$INSTANCE_NAME`_bI2C_UDB_Shifter_u0__A0_REG)
+#define `$INSTANCE_NAME`_GO_REG         (*(reg8 *) `$INSTANCE_NAME`_bI2C_UDB_Shifter_u0__F1_REG)
 
 void `$INSTANCE_NAME`_SetupDMA(uint8 max_len);
 void `$INSTANCE_NAME`_SetupIRQ(void);
